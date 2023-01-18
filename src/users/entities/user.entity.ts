6b0513df2817d2078aa8ab9d05ff17dba75f2634
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-01-18 18:50:44 +0200

    feat: wish entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,17 @@
-import { IsString, Min, Max, IsEmail, IsUrl } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsString, Min, Max, IsEmail, IsUrl, IsDate } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import {
   AVATAR_DEFAULT_LINK,
   ABOUT_DEFAULT_TEXT,
 } from 'src/utils/constants/users';
+import { Wish } from 'src/wishes/entities/wish.entity';
 
 @Entity()
 export class User {
@@ -34,9 +42,18 @@ export class User {
   @IsString()
   password: string;
 
+  @OneToMany(() => Wish, (wish) => wish.owner)
   wishes: [];
 
   offers: [];
 
   wishlists: [];
+
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @IsDate()
+  updatedAt: Date;
 }
--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,68 @@
-export class Wish {}
+import { User } from 'src/users/entities/user.entity';
+import { IsDate, IsString, Min, Max, IsUrl, IsNumber, IsInt } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Wish {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsString()
+  @Min(2)
+  @Max(250)
+  name: string;
+
+  @Column()
+  @IsString()
+  @Min(1)
+  @Max(1024)
+  description: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column({
+    type: 'numeric',
+    scale: 2,
+  })
+  @IsNumber()
+  price: number;
+
+  @Column({
+    type: 'numeric',
+    scale: 2,
+    default: 0,
+  })
+  @IsNumber()
+  raised: number;
+
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  offers: [];
+
+  @Column({ default: 0 })
+  @IsInt()
+  copied: number;
+
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @IsDate()
+  updatedAt: Date;
+}
